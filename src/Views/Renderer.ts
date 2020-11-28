a281c33d8e168f58efdebdd9f9a17ac0e05aa70f
Oryam <48219965+or-yam@users.noreply.github.com>
2020-11-28 21:23:12 +0200

    features added

--- src/Views/Renderer.ts
+++ src/Views/Renderer.ts
@@ -1,39 +1,149 @@
 class Renderer {
+  //insert coin info into card from coin API data-Coin
   renderCoinCard(coinData: Coin) {
+    let checked = coinData.isSaved ? 'checked' : null;
     $('.coins-container').append(`
-    <div id=${coinData.id}  class="card m-4" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">${coinData.id}</h5>
+    <div data-id="${coinData.id}" id="${
+      coinData.id
+    }" class="card m-4" style="width: 18rem;">
+        <div class="card-body main-card">
+            <h5 class="card-title">${coinData.symbol.toUpperCase()}</h5>
             <p class="card-text">${coinData.name}</p>
-            <a  href="#" class="btn btn-primary">More Info</a>
+            <a   href="#<?php echo("a".$cntr); ? class="btn-moreInfo btn btn-primary"><?php echo($cntr); ?>More Info</a>
             <p  class="card-text more-info"></p>
             <div class="custom-control custom-switch">
-                <input type="checkbox" class="custom-control-input" id="customSwitch1">
-                <label class="custom-control-label" for="customSwitch1">Toggle</label>
+                <input type="checkbox" class="custom-control-input " id="customSwitch${
+                  coinData.id
+                }" ${checked}>
+                <label class="custom-control-label" for="customSwitch${
+                  coinData.id
+                }">Select Coin</label>
             </div>
         </div>
     </div>
     `);
   }
 
-  renderCoinsList(list: Coin[]) {
+  //render coins into cards
+  renderCoinsList(CoinsList: Coin[]) {
     $('.coins-container').empty();
-    for (const coinData of list) {
+    for (const coinData of CoinsList) {
       this.renderCoinCard(coinData);
     }
   }
 
+  //loading while waiting for API to respond
   renderCoinsLoading() {
     $('.coins-container').empty();
-    $('.coins-container').append(`<div>Loading Coins Data...</div>`);
+    $('.coins-container').append(`
+            <div class="spinner-border text-success" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        `);
   }
-
-  renderMoreInfo(id: Coin['id'], info: Coin['data']) {
+  //loading while waiting for API to respond
+  renderCoinDataLoading(id: string) {
+    const infoButton = $(`#${id}`).find('.btn-moreInfo');
+    infoButton.empty();
+    infoButton.append(`
+        <span class="spinner-border spinner-border-sm"></span>
+        Loading..
+        `);
+  }
+  //loading while waiting for API to respond
+  clearLoading(id: string) {
+    const infoContainer = $(`#${id}`).find('.more-info');
+    infoContainer.empty();
+    const infoButton = $(`#${id}`).find('.btn-moreInfo');
+    infoButton.empty();
+    infoButton.append(`
+        <a  href="#" class="btn-moreInfo btn btn-primary">More Info</a>
+        `);
+  }
+  // render more info into collapse card
+  renderMoreInfo(id: Coin['id'], coin: Coin['data']) {
     const infoContainer = $(`#${id}`).find('.more-info');
     infoContainer.empty().append(`
-    <div>
-    <img src=${info.image} alt="icon">    
-    <span>${info.marketData[0]}$</span>
-    </div>`);
-  } //make a real template
-}
+            <div>
+                <center><img class="coin-img"src=${coin.image}  alt="icon"></center>  
+            <table id="coin-table">
+                <tr>
+                    <th>Dollar</th>
+                    <th>Shekel</th> 
+                    <th>Euro</th>
+                </tr>
+                <tr>
+                    <td> $ ${coin.marketData[0]} &nbsp</td>
+                    <td> &#8362 ${coin.marketData[1]} &nbsp</td>
+                    <td> &#8364 ${coin.marketData[2]} &nbsp</td>
+                </tr>
+            </table>
+            </div>
+        `);
+  }
+  //render popup-modal for selected coins and  call render selected coins and last coin
+  renderSelectedCoinsList(selectedList: any) {
+    $('.modal-content').empty();
+    $('.coins-container').append(`
+        <div id="myModal" class="modal">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h2>You have to remove one coin from the list </h2>
+                    <p class='modal-message'>The maximum amount of coins to compare is 5</p>
+                        <div class="modal-buttons">
+                    <button class="close">Cancel</button>
+                        </div>
+                </div>  
+                    </div class="modal-coins">  </div>    
+               
+            </div>
+        </div>
+    `);
+    selectedList.favorites.map((coin: Coin) => {
+      selectedList.coin.id === coin.id
+        ? this.renderLastAdded(coin)
+        : this.renderSelectedCoinsCard(coin);
+    });
+  }
+  // Render the last coin
+  renderLastAdded(coin: Coin) {
+    $('.modal-message').append(`<span> ${coin.name} was just selected </span>`);
+  }
+
+  // Render selected coins from main-coins-page into selected pop-up modal
+  renderSelectedCoinsCard(selectedCoinData: Coin) {
+    $('.modal-content').append(`
+            <div class="modal-card" data-id="${selectedCoinData.id}">
+                <div class="modal-card-body">
+                    <h5 class="modal-card-title">${selectedCoinData.symbol.toUpperCase()}</h5>
+                    <p class="modal-card-text">${selectedCoinData.name}</p>
+                    <div class="modal-custom-control modal-custom-switch">
+                        <input type="checkbox" class="modal-custom-control-input " id="customSwitch2${
+                          selectedCoinData.id
+                        }" checked>
+                        <label class="modal-custom-control-label" for="customSwitch2${
+                          selectedCoinData.id
+                        }">Select Coin</label>
+                    </div>
+                </div>
+            </div>
+        `);
+    this.displayTooMuchCoins();
+  }
+  //Display Selected coin modal
+  displayTooMuchCoins() {
+    $('#myModal').css('display', 'block');
+  }
+  //Render about page
+  renderAboutPage() {
+    $('.about-container').append(`
+        <div class="selfi-container">
+            <img src="../assets/images/selfi.jpg"  width="300" height="400" class="d-inline-block align-top" alt="">
+        </div>
+        <div class="siteDescription-container">
+            <p>My name is Lev Berger I'm the developer of this single page crypto coin App </p>
+            <p>This App is build using TypeScript, JavaScript, Jquery, Bootstrap, CSS and HTML </p>
+        </div>
+ `);
+  }
+} //End
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-type Coin = {
+//TS types
+interface Coin {
   id: string;
   name: string;
   symbol: string;
@@ -6,26 +7,175 @@ type Coin = {
   data: {
     marketData: number[];
     image: string;
-    dateIssued: {};
+    dateIssued: Date;
   };
+}
+
+interface GraphCoin {
+  x: Date;
+  y: number;
+}
+
+interface GraphCoins {
+  [key: string]: [GraphCoin];
+}
+
+interface ChartData {
+  type: 'line';
+  showInLegend: true;
+  name: string;
+  markerType: 'square';
+  dataPoints: GraphCoin[];
+}
+
+//global variables
+const apiManager = new ApiManager();
+const renderManager = new Renderer();
+let chart: any = {};
+let interval: any;
+
+//search box function
+const onSearchSubmit = () => {
+  let text = $('#search').val()?.toString().toLocaleLowerCase();
+  const filteredCoins = apiManager.data.coins.filter(
+    (coin) => coin.symbol === text
+  );
+
+  text === ''
+    ? alert('Enter Coin Symbol')
+    : filteredCoins.length === 0
+    ? alert('Did not find Coin Symbol')
+    : null;
+
+  filteredCoins.length && renderManager.renderCoinsList(filteredCoins);
 };
 
-const coinsManager = new ApiManager();
-const viewManager = new Renderer();
-
+// call display coins from API to Render
 const displayCoins = async () => {
-  viewManager.renderCoinsLoading();
-  let list: Coin[] = await coinsManager.getCoinsList();
-  viewManager.renderCoinsList(list);
+  renderManager.renderCoinsLoading();
+  let list: Coin[] = await apiManager.getCoinsList();
+  renderManager.renderCoinsList(list);
 };
-displayCoins();
 
+//display coin-info by ID from button
 const displayMoreData = async (id: string) => {
-  let coin: Coin | undefined = await coinsManager.getCoinInfo(id);
-  coin ? viewManager.renderMoreInfo(id, coin.data) : undefined;
+  renderManager.renderCoinDataLoading(id);
+  let coin: Coin | undefined = await apiManager.getCoinInfo(id);
+  coin ? renderManager.renderMoreInfo(id, coin.data) : undefined;
 };
 
-$('.coins-container').on('click', '.btn', function () {
-  let id = $(this).closest('.card').attr('id');
-  id ? displayMoreData(id) : undefined;
+//call function to load modal for more than 5 coins selected by ID
+const handelSelectedCoins = (id: any, value: any) => {
+  let selectedCoins = apiManager.toggleSave(id, value);
+  if (selectedCoins.favorites.length === 6) {
+    renderManager.renderSelectedCoinsList(selectedCoins);
+    selectedCoins.coin.isSaved = false;
+  }
+};
+
+//handel the state of More Info (display and set time after first click API call)
+const handleMoreInfoData = async (id: string) => {
+  let coin: Coin | undefined = apiManager.data.coins.find((c) => c.id === id);
+  if (coin?.data) {
+    let issuedDate = coin.data.dateIssued.getTime();
+    let currentDate = new Date().getTime();
+    let delay = currentDate - issuedDate;
+    if (delay > 12000) {
+      let coin = await apiManager.getCoinInfo(id);
+      return coin.data;
+    } else {
+      return coin.data;
+    }
+  } else {
+    coin = await apiManager.getCoinInfo(id);
+    return coin.data;
+  }
+};
+
+const onCloseModal = () => {
+  $('#myModal').css('display', 'none');
+  let coinsData = apiManager.data.coins;
+  renderManager.renderCoinsList(coinsData);
+};
+
+const showCoinsScreen = () => {
+  $('.coins-container').empty();
+  $('.about-container').empty();
+  $('#reports-container').empty();
+  let coinsData = apiManager.data.coins;
+  renderManager.renderCoinsList(coinsData);
+  clearInterval(interval);
+};
+
+const showAboutScreen = () => {
+  $('.coins-container').empty();
+  $('.about-container').empty();
+  $('#reports-container').empty();
+  renderManager.renderAboutPage();
+  clearInterval(interval);
+};
+
+const updateChart = async () => {
+  await apiManager.getDataToCanvas();
+  chart.render();
+};
+
+const showRealTimeScreen = async () => {
+  $('.coins-container').empty();
+  $('.about-container').empty();
+  $('#reports-container').empty();
+  console.log('before clear :', apiManager.data.chartOptions);
+  apiManager.data.chartOptions.data = [];
+  console.log('after clear :', apiManager.data.chartOptions);
+  await apiManager.getDataToCanvas();
+  chart = new CanvasJS.Chart('reports-container', apiManager.data.chartOptions);
+  chart.render();
+  interval = setInterval(updateChart, 2000);
+};
+
+// on click "Main selected coin toggle" and send data-id from btn(actually from main div coin-card) and value from toggle to handelSelectedCoins
+$('.coins-container').on(
+  'click',
+  '.custom-control-input',
+  function (event): void {
+    let id: string = $(this).closest('.card').attr('data-id')!;
+    let value: boolean = event.currentTarget.checked;
+    handelSelectedCoins(id, value);
+  }
+);
+
+//on click "More info" send id from btn(actually from main div coin-card) to displayMoreData(id) & saveToLocalStorage(id)
+$('.coins-container').on('click', '.btn', async function () {
+  let id = $(this).closest('.card').attr('data-id')!;
+  if ($(this).next('.more-info').css('display') === 'none') {
+    renderManager.renderCoinDataLoading(id);
+    let coinData: Coin['data'] = await handleMoreInfoData(id!);
+    renderManager.clearLoading(id);
+    id && renderManager.renderMoreInfo(id, coinData);
+    $(this).next('.more-info').css('display', 'block');
+  } else {
+    $(this).next('.more-info').css('display', 'none');
+  }
 });
+
+// on click "Modal selected coin toggle" and change Coin.isSaved value in main coin array by id
+$('.coins-container').on(
+  'click',
+  '.modal-custom-control-input',
+  function (event): void {
+    let id: string = $(this).closest('.modal-card').attr('data-id')!;
+    let value: boolean = event.target.checked ? true : false;
+    let coin: Coin = apiManager.data.coins.find(
+      (coin: Coin) => coin.id === id
+    )!;
+    coin.isSaved = value;
+  }
+);
+
+$('.navbar').on('click', '.search-btn', onSearchSubmit);
+$('.coins-container').on('click', '.close', onCloseModal);
+$('#coins-tab').on('click', showCoinsScreen);
+$('#about-tab').on('click', showAboutScreen);
+$('#realTime-tab').on('click', showRealTimeScreen);
+
+displayCoins();
